92de7d02c1d1dbde7cbf81811f4324d47289ddda
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-12-12 21:19:26 +0100

    update

--- src/routes/api/posts/index.ts
+++ src/routes/api/posts/index.ts
@@ -1,5 +1,29 @@
 import { RequestHandler } from "@builder.io/qwik-city";
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+
+const addPostSchema = z.object({
+  name: z.string(),
+  teaser: z.string(),
+  url: z.string(),
+  content: z.string(),
+});
 
 export const onGet: RequestHandler = async () => {
-  return { value: "Hallo von Qwik" };
+  const prisma = new PrismaClient();
+  const dbPosts = await prisma.post.findMany();
+  return dbPosts.map(({ id, name, teaser, url, content }) => ({
+    id,
+    name,
+    teaser,
+    url,
+    content,
+  }));
+};
+
+export const onPost: RequestHandler = async ({ request }) => {
+  const body = await request.json();
+  const addPost = addPostSchema.parse(body);
+  const prisma = new PrismaClient();
+  await prisma.post.create({ data: addPost });
 };
---
+++ test.sh
@@ -0,0 +1,1 @@
+curl -X POST http://localhost:5173/api/posts -H "Content-Type: application/json" -d '{"name": "Test", "url": "test", "content": "Das ist ein Testpost", "teaser": "Qwik Test"}' 
